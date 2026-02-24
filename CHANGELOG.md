ddef763eafa73d66f2f00d2e2375b4b9ac2be6c9
Patrici <patrisiu@gmail.com>
2026-02-24 23:42:12 +0100

    Web Release 1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,21 +5,25 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.1.0] - 2026-02-25
+
+### Added
+
+- AI model Gemini 2.5 Flash Lite for Stocks and Trends analysis.
+
 ## [1.0.5] - 2026-02-05
 
 ### Added
 
 - Configure Price Alerts week days to be executed.
 
-### Fixed
-
-- Price Alerts background task with last execution report.
-
-## [1.0.4] - 2026-02-01
-
 ### Changed
 
 - Restored GoogleSignIn for user login.
+
+### Fixed
+
+- Price Alerts background task with last execution report.
 
 ### Deleted
 
--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"2beceb6b1c0701ce0d3289dec41c7fa7f8678b9e","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"fafe9c3c1e772019a4894f6c3092f3f633654b1f","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2075031600" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2652119157" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
